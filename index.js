98e6158e4cd1ae2c33932c1f65285a83f47ec9d2
treisse93 <misstresse.slt@hotmail.fr>
2023-02-10 15:24:06 +0100

    Ajout photo Agurimis

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const nav = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 500) {
+    if (window.scrollY > 250) {
         nav.style.top = 0;
     } else {
-        nav.style.top = "-500px"
+        nav.style.top = "-250px"
     }
 });
